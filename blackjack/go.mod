4bd59224676ce226206f355dc4d44a73c5550e1d
York Yao <userid@bucknell.edu>
2022-12-11 01:48:35 -0800

    blackjack without AI

--- blackjack/go.mod
+++ blackjack/go.mod
@@ -1,3 +1,5 @@
-module yldoge.com/blackjack
+module github.com/yldoge/gophercises/blackjack
 
 go 1.19
+
+require github.com/yldoge/gophercises/deck v0.0.0-20221211091410-48761f17083e // indirect
--- blackjack/main.go
+++ blackjack/main.go
@@ -2,15 +2,52 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
-	"yldoge.com/deck"
+	"github.com/yldoge/gophercises/deck"
 )
+
+type Hand []deck.Card
+
+func (h Hand) String() string {
+	strs := make([]string, len(h))
+	for i := range h {
+		strs[i] = h[i].String()
+	}
+	return strings.Join(strs, ", ")
+}
+
+func (h Hand) DealerString() string {
+	return h[0].String() + ", **HIDDEN**"
+}
 
 func main() {
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
 	var card deck.Card
-	for i := 0; i < 10; i++ {
-		card, cards = cards[i], cards[1:]
-		fmt.Println(card)
+	var player, dealer Hand
+	for i := 0; i < 2; i++ {
+		for _, hand := range []*Hand{&player, &dealer} {
+			card, cards = draw(cards)
+			*hand = append(*hand, card)
+		}
 	}
+	var input string
+	for input != "s" {
+		fmt.Println("Player: ", player)
+		fmt.Println("Dealer: ", dealer.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, cards = draw(cards)
+			player = append(player, card)
+		}
+	}
+	fmt.Println("==FINAL HANDS==")
+	fmt.Println("Player: ", player)
+	fmt.Println("Dealer: ", dealer)
+}
+
+func draw(cards []deck.Card) (deck.Card, []deck.Card) {
+	return cards[0], cards[1:]
 }
