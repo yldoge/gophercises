2360bc57251266a82be41b3231f345cc598ecda6
York Yao <userid@bucknell.edu>
2022-12-11 02:10:11 -0800

    added simple AI for dealer in blackjack

--- blackjack/main.go
+++ blackjack/main.go
@@ -21,6 +21,39 @@ func (h Hand) DealerString() string {
 	return h[0].String() + ", **HIDDEN**"
 }
 
+// real score that may treats Ace as 11
+func (h Hand) Score() int {
+	minScore := h.MinScore()
+	if minScore > 11 {
+		return minScore
+	}
+	for _, c := range h {
+		// change the first Ace for 1 to 11
+		// add 10 to original score
+		// return immediately since score > 11
+		if c.Rank == deck.Ace {
+			return minScore + 10
+		}
+	}
+	return minScore
+}
+
+// score of hand that treats Ace as 1
+func (h Hand) MinScore() int {
+	score := 0
+	for _, card := range h {
+		score += min(int(card.Rank), 10)
+	}
+	return score
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func main() {
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
 	var card deck.Card
@@ -43,9 +76,29 @@ func main() {
 			player = append(player, card)
 		}
 	}
+	// Add a simple AI for dealer
+	// if dealer score <= 16, it hits
+	// or if dealer has a soft 17, it hits
+	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
+		card, cards = draw(cards)
+		dealer = append(dealer, card)
+	}
+	pScore, dScore := player.Score(), dealer.Score()
 	fmt.Println("==FINAL HANDS==")
-	fmt.Println("Player: ", player)
-	fmt.Println("Dealer: ", dealer)
+	fmt.Println("Player: ", player, "\nScore: ", pScore)
+	fmt.Println("Dealer: ", dealer, "\nScore: ", dScore)
+	switch {
+	case pScore > 21:
+		fmt.Println("You busted...")
+	case dScore > 21:
+		fmt.Println("Dealer busted, you win!")
+	case pScore > dScore:
+		fmt.Println("You win!")
+	case pScore < dScore:
+		fmt.Println("You lose...")
+	case pScore == dScore:
+		fmt.Println("Draw.")
+	}
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
